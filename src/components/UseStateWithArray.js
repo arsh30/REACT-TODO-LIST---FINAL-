52907d276e4be4f6a74148146018deb17327a246
Arsh <as3096734@gmail.com>
2023-01-25 02:13:45 +0530

    update upto delete individual entry of todo

--- src/components/UseStateWithArray.js
+++ src/components/UseStateWithArray.js
@@ -13,16 +13,33 @@ const UseStateWithArray = () => {
     // render hoye
     setMyArr([]);
   };
+
+  const removeElem = (id) => {
+    // alert(id);
+    const myNewArr = myArr.filter((elem) => {
+      return elem.id !== id;
+      //   means jo upr se id vo har elem ki id ke equal nahi hai bss vo aayegi
+    });
+
+    // console.log(myNewArr);
+    setMyArr(myNewArr);
+  };
   return (
     <div>
       {myArr.map((elem, index) => {
         return (
           <h1 key={elem.id}>
             Name: {elem.name} & Age:{elem.age}
+            <button
+              style={{ margin: "6px" }}
+              onClick={() => removeElem(elem.id)}
+            >
+              Remove elem
+            </button>
           </h1>
         );
       })}
-      <button onClick={clearData}>clear</button>
+      <button onClick={() => clearData()}>clear</button>
       {/* <h1>Name:Arshdeep & Age:26</h1> */}
     </div>
   );
